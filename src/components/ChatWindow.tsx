034eee400209b458c36e19e95b8abaf77c856c12
Olamigokeolowo <covenantolamigoke69@gmail.com>
2025-07-21 21:57:36 +0100

    Add location permission popup and message display

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -34,7 +34,7 @@ function ChatWindow() {
     setMessages((prevMessages) => [...prevMessages, { sender: "user", text: message }]);
 
     // 2. Optionally send to backend (you can add this later)
-    fetch("http://localhost:5000/message", {
+    fetch("https://trafficchatter.onrender.com", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message }),
