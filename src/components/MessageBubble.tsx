23c7255f73c2d96357003f8b2847007bcd3b83e5
tedph <theooye7@gmail.com>
2025-07-31 16:07:15 +0100

    response interface

--- src/components/MessageBubble.tsx
+++ src/components/MessageBubble.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import bot_avatar from '../images/bot-avatar.png';
 import user_avatar from '../images/user-avatar.png';
+import ReactMarkdown from 'react-markdown';
 type Props = {
-  sender: "bot" | "user"; // this restricts sender to only these two strings
+  sender: "bot" | "user";
   text: string;
 };
 
@@ -10,10 +11,20 @@ function MessageBubble ({ sender, text }: Props)  {
   const isBot = sender === "bot";
 
   return (
-    <div className='flex p-2 items-start gap-2'>
-      <img src={isBot ? bot_avatar : user_avatar} alt={isBot ? "Bot" : "User"} className='rounded-full w-8 h-8 aspect-auto' />
-      <div className={`p-2 rounded-lg w-fit ${isBot ? 'bg-blue-100' : 'bg-green-100'} text-black`}>
-        <p>{text}</p>
+    <div className="flex p-2 items-start gap-2">
+      <img
+        src={isBot ? bot_avatar : user_avatar}
+        alt={isBot ? "Bot" : "User"}
+        className="rounded-full w-8 h-8 aspect-auto"
+      />
+      <div
+        className={`p-2 rounded-lg w-fit ${
+          isBot ? "bg-blue-100" : "bg-green-100"
+        } text-black`}
+      >
+        <p>
+          <ReactMarkdown>{text}</ReactMarkdown>
+        </p>
       </div>
     </div>
   );
